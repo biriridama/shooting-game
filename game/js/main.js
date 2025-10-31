fcc3bf69ae8d56aa837cc7b1da0013f66eaaaa23
uketu <bbp-510-rwm@e-net.nara.jp>
2025-10-31 06:20:05 +0000

    衝突判定の追加

--- game/js/main.js
+++ game/js/main.js
@@ -1,49 +1,51 @@
-import { player , initPlayer, drawPlayer } from "./player.js";
-import { spawnEnemy , enemies } from "./enemies.js";
+import { player, initPlayer, drawPlayer } from "./player.js";
+import { spawnEnemy, enemies, updateEnemies, drawEnemies } from "./enemies.js";
+import { handleCollisions } from "./collision.js";
 
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
 initPlayer(canvas);
-spawnEnemy(canvas);
 
-const bullets =[];
-const BULLET_SPEED = -10;
+export const bullets = [];
+const BULLET_SPEED = -5;
 
-function tryShoot(now) {
+function tryShoot() {
     bullets.push({
-        x: player.x,
+        x: player.x+player.width/2 - 5,
         y: player.y,
-        width: 20,
-        height:20,
+        width: 5,
+        height: 5,
         vy: BULLET_SPEED,
     })
 }
 
 
-//自分のキャラクター操作
-window.addEventListener("keydown" , (e) => {
-    if(e.key === "ArrowLeft") {
+window.addEventListener("keydown", (e) => {
+    if (e.key === "ArrowLeft") {
         if (player.x > 10) {
             player.x -= 10;
         }
-    } else if(e.key === "ArrowRight") {
+    } else if (e.key === "ArrowRight") {
         if (player.x < canvas.width - player.width - 10) {
             player.x += 10;
         }
-    } else if(e.code === "Space") {
+    } else if (e.code === "Space") {
         tryShoot();
     }
 });
 
 function update() {
-     for (let i = 0; i < bullets.length; i++) {
+    for (let i = 0; i < bullets.length; i++) {
         const bullet = bullets[i];
-        bullet.y += bullet.vy; 
-         if (bullet.y < 0) {
+        bullet.y += bullet.vy;
+        if (bullet.y < 0) {
             bullets.splice(i, 1);
         }
-     }
+    }
+    spawnEnemy(canvas);
+    updateEnemies(canvas);
+    handleCollisions();
 }
 
 function draw() {
@@ -63,21 +65,10 @@ function draw() {
         const enemy = enemies[i];
         ctx.fillRect(enemy.x, enemy.y, enemy.width, enemy.height);
     }
+    drawEnemies(ctx);
 }
 
-
 function gameLoop() {
-    /*ctx.fillStyle = "blue";
-    ctx.fillRect(75,75,canvas.width-150, canvas.height-150);
-    ctx.fillStyle = "lightblue";
-    ctx.fillRect(0,0,75,640);
-    ctx.fillStyle = "lightblue";
-    ctx.fillRect(0,0,640,75);
-    ctx.fillStyle = "lightblue";
-    ctx.fillRect(405,0,75,640);
-    ctx.fillStyle = "lightblue";
-    ctx.fillRect(0,565,480,75);*/
-
     update();
     draw();
     requestAnimationFrame(gameLoop);
